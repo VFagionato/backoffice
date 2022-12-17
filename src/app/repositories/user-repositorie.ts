e9877c5d4eebc017502646de4b57eef98858f5f9
Viktor Fagionato <viktormfagionato@gmail.com>
2022-12-17 00:33:30 -0300

    feat: create user repository

--- src/app/repositories/user-repositorie.ts
+++ src/app/repositories/user-repositorie.ts
@@ -1,10 +1,10 @@
 import { User } from '../entities/user';
 
 export abstract class UserRepository {
-  abstract create(user: User): Promise<void>;
+  abstract create(user: User): Promise<User>;
   abstract findByID(userID: string): Promise<User | null>;
   abstract findByEmail(userEmail: string): Promise<User | null>;
-  abstract listAlluser(): Promise<User[]>;
-  abstract save(user: User): Promise<void>;
+  abstract listAllusers(): Promise<User[]>;
+  abstract update(user: User): Promise<void>;
   abstract deleteUser(userID: string): Promise<void>;
 }
--- src/infra/prisma/repositories/prisma-user-repository.ts
+++ src/infra/prisma/repositories/prisma-user-repository.ts
@@ -2,7 +2,7 @@ import { User } from 'src/app/entities/user';
 import { UserRepository } from 'src/app/repositories/user-repositorie';
 
 export class PrismaUserRepository implements UserRepository {
-  create(user: User): Promise<void> {
+  create(user: User): Promise<User> {
     throw new Error('Method not implemented.');
   }
   findByID(userID: string): Promise<User | null> {
@@ -11,10 +11,10 @@ export class PrismaUserRepository implements UserRepository {
   findByEmail(userEmail: string): Promise<User | null> {
     throw new Error('Method not implemented.');
   }
-  listAlluser(): Promise<User[]> {
+  listAllusers(): Promise<User[]> {
     throw new Error('Method not implemented.');
   }
-  save(user: User): Promise<void> {
+  update(user: User): Promise<void> {
     throw new Error('Method not implemented.');
   }
   deleteUser(userID: string): Promise<void> {
