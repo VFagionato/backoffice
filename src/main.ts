112c96c4e5f0b4eba18d4672512bb97244825cfc
Viktor Fagionato <viktormfagionato@gmail.com>
2022-12-24 14:28:16 -0300

    chore: prepare app for deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug'],
+    logger: ['log'],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
