ccdef7519452c4f19a7f3dd09600c411a5f49f6b
Viktor Fagionato <viktormfagionato@gmail.com>
2022-12-28 14:10:07 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log'],
+    cors: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
